9813ae014dce7a941045dbecc976e26fbc4b5289
aysebat <aaysebatt@gmail.com>
2023-01-03 08:16:55 +0300

    added genereta method in PdfReport class

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contain data about a bill,
@@ -26,12 +28,38 @@ class PdfReport:
     such as their names, their due amount and the period
     of the bill.
     """
-
-    def __init__(self, days_in_house):
-        self.days_in_house = days_in_house
+    def __init__(self, filename):
+        self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+
+        pdf = FPDF(orientation='p', unit='pt', format='A4')
+        pdf.add_page()
+
+        #Add the title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill Report', border=1, align='C', ln=1)
+
+        #add the period and value
+        pdf.cell(w=100, h=40, txt="Period ", border=0)
+        pdf.cell(w=10, h=40, txt=":", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        #add flatmates1
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=10, h=40, txt=":", border=0)
+        pdf.cell(w=100, h=40, txt=flatmate1_pay, border=0, ln=1)
+
+        # add flatmates2
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=10, h=40, txt=":", border=0)
+        pdf.cell(w=100, h=40, txt=flatmate2_pay, border=0, ln=1)
+
+
+        pdf.output(self.filename)
 
 
 the_bill = Bill(amount=120, period="March 2021")
@@ -39,4 +67,7 @@ john = Flatmate(name="John", days_in_house=20)
 marry = Flatmate(name="Marry", days_in_house=25)
 
 print("Johns Pays",john.pays(bill=the_bill, flatmate2=marry))
-print("MArry Pays",marry.pays(bill=the_bill, flatmate2=john))
+print("Marry Pays",marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename='Report1.pdf')
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)

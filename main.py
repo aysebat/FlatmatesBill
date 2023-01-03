d55896c7d47da6f582f1223da11d13826b7eeac5
aysebat <aaysebatt@gmail.com>
2023-01-03 07:17:14 +0300

    added pay method

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ class Flatmate:
     def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house    
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house /(self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return to_pay
 
 class PdfReport:
     """
@@ -32,3 +34,9 @@ class PdfReport:
         pass
 
 
+the_bill = Bill(amount=120, period="March 2021")
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate(name="Marry", days_in_house=25)
+
+print("Johns Pays",john.pays(bill=the_bill, flatmate2=marry))
+print("MArry Pays",marry.pays(bill=the_bill, flatmate2=john))

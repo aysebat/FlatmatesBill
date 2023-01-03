816e53cc6e2318356a33acc23e36fd6eb0945a46
aysebat <aaysebatt@gmail.com>
2023-01-03 08:24:58 +0300

    added icon image to the PDFreport class

--- main.py
+++ main.py
@@ -39,23 +39,31 @@ class PdfReport:
         pdf = FPDF(orientation='p', unit='pt', format='A4')
         pdf.add_page()
 
+        #Add icon
+        pdf.image("files/house.png", w=30, h=30)
+
         #Add the title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt='Flatmates Bill Report', border=1, align='C', ln=1)
+        pdf.cell(w=0, h=80, txt='Flatmates Bill Report', border=0, align='C', ln=1)
 
         #add the period and value
+        pdf.set_font(family='Times', size=18, style='B')
         pdf.cell(w=100, h=40, txt="Period ", border=0)
         pdf.cell(w=10, h=40, txt=":", border=0)
         pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         #add flatmates1
+        pdf.set_font(family='Times', size=18, style='B')
         pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
         pdf.cell(w=10, h=40, txt=":", border=0)
+        pdf.set_font(family='Times', size=18)
         pdf.cell(w=100, h=40, txt=flatmate1_pay, border=0, ln=1)
 
         # add flatmates2
+        pdf.set_font(family='Times', size=18, style='B')
         pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
         pdf.cell(w=10, h=40, txt=":", border=0)
+        pdf.set_font(family='Times', size=18)
         pdf.cell(w=100, h=40, txt=flatmate2_pay, border=0, ln=1)
 
 

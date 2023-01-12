adc47f33db0c6cdda7720f6373f4b2cb3b70785a
aysebat <aaysebatt@gmail.com>
2023-01-12 06:57:27 +0300

    update the flask app, added the home page and bill page to the program

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
+
 class HomePage(MethodView):
-    pass
+    def get(self):
+        return "Hello"
 
 
 class BillFormPage(MethodView):
@@ -14,3 +19,9 @@ class ResultsPage(MethodView):
 
 class BillForm(Form):
     pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run(host='0.0.0.0', debug=True)
